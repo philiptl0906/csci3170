19fba9e11d1f6339392a7062f9ddfb3acb188049
wmhui0102 <1155127512@link.cuh.edu.hk>
2021-04-03 17:45:24 +0800

    choices for Bookstore

--- Bookstore.java
+++ Bookstore.java
@@ -1,3 +1,59 @@
-public class Bookstore {
+import java.util.*;
+import java.io.*;
+
     // test
+public class Bookstore{
+    public static void run(Scanner in){
+        int input= 0;
+        run: while(true){
+            System.out.println("----------------------------------");
+            System.out.println("1. Order Update.");
+            System.out.println("2. Order Query.");
+            System.out.println("3. N most Popular Book Query.");
+            System.out.println("4. Back to main menu");
+            System.out.println(" ");
+            System.out.println("What is your choice??..");
+            
+            try {
+                input = in.nextInt();
+            }
+
+            catch(Exception e){
+                input = 0 ;
+            }
+
+            finally{
+                switch(input){
+                    case 1:
+                        OrUpdate();
+                        break;
+                    case 2:
+                        OrQuery();
+                        break;
+                    case 3: 
+                        Npop();
+                        break;
+                    case 4: 
+                        break run;
+                    default:
+                        System.out.println("Invaild choice.");
+                        break;
+                }
+            }   
+        }
+    }
+
+    private static void OrUpdate(){
+
+    }
+
+    private static void OrQuery(){
+
+    }
+
+    private static void Npop(){
+
+    }
+
+    
 }
