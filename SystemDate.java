697beb360cbbb9d97bc23ed9e66b649ad2579977
Julianna <julianna@Vania-Cheung-de-MacBook-Air.local>
2021-04-05 00:15:02 +0800

    add systemDate class and set a system date for main class

---
+++ SystemDate.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+
+public class SystemDate {
+    public static void run(Date sysDate) {
+        System.out.println("The System Date is now: " + sysDate);
+
+    }
+}
--- main.java
+++ main.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         boolean applicationIsRunning = true;
         Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date sysDate = sdf.parse("0000-00-00");
 
         while (applicationIsRunning) {
-            printMenu();
+            printMenu(sysDate);
 
             boolean inputIsValid = false;
 
@@ -16,28 +21,28 @@ public class Main {
                     int userAccount = scanner.nextInt();
 
                     switch (userAccount) {
-                    case 1:
-                        System.run(scanner);
-                        inputIsValid = true;
-                        break;
-                    case 2:
-                        Customer.run(scanner);
-                        inputIsValid = true;
-                        break;
-                    case 3:
-                        Bookstore.run(scanner);
-                        inputIsValid = true;
-                        break;
-                    case 4:
-                        SystemDate.run(scanner);
-                        inputIsValid = true;
-                        break;
-                    case 5:
-                        applicationIsRunning = false;
-                        inputIsValid = true;
-                        break;
-                    default:
-                        System.err.println(Error.INVALID_INPUT);
+                        case 1:
+                            System.run(scanner);
+                            inputIsValid = true;
+                            break;
+                        case 2:
+                            Customer.run(scanner);
+                            inputIsValid = true;
+                            break;
+                        case 3:
+                            Bookstore.run(scanner);
+                            inputIsValid = true;
+                            break;
+                        case 4:
+                            SystemDate.run(sysDate);
+                            inputIsValid = true;
+                            break;
+                        case 5:
+                            applicationIsRunning = false;
+                            inputIsValid = true;
+                            break;
+                        default:
+                            System.err.println(Error.INVALID_INPUT);
                     }
 
                 } catch (Exception e) {
@@ -51,8 +56,8 @@ public class Main {
         scanner.close();
     }
 
-    public static void printMenu() {
-        System.out.println("The system date is now: 0000-00-00");
+    public static void printMenu(Date sysDate) {
+        System.out.println("The system date is now: " + sysDate);
         System.out.println("<This is the book ordering system.>");
         System.out.println("----------------------------------");
         System.out.println("1. System interface");
